6caef57db5b3755e38684eea7c145115d761b6d6
Ankit Prajapati <ap8424856@gmail.com>
2025-12-07 01:22:58 +0530

    added base16 color

--- machines/nidhivan/stylix.nix
+++ machines/nidhivan/stylix.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
     stylix.autoEnable = true;
+    # Base16 color scheme - you can change this to any base16 theme
+    # Popular options: "gruvbox-dark-hard", "nord", "dracula", "tokyo-night-dark", "catppuccin-mocha"
+    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
     stylix.cursor = {
         package = pkgs.bibata-cursors;
         name = "Bibata-Modern-Ice";
